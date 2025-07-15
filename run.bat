a3f7756f6c72fb8ccd558e55eade895325678f73
pouoq <3323249236@qq.com>
2025-07-15 12:15:26 +0800

    项目整理完毕

--- run.bat
+++ run.bat
@@ -2,4 +2,4 @@
 REM 设置 PowerShell 的输出编码为 UTF-8，并运行 test.exe
 
 powershell -NoLogo -NoProfile -Command ^
- "$OutputEncoding = [Console]::OutputEncoding = [System.Text.UTF8Encoding]::new(); .\test.exe"
+ "$OutputEncoding = [Console]::OutputEncoding = [System.Text.UTF8Encoding]::new(); .\build\main.exe"
