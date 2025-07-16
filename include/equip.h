941a9e716838140d3fce76ae19e250596da982e4
Faker <decadelfy@163.com>
2025-07-16 15:54:45 +0800

    武器防具饰品初步制作完了

--- include/equip.h
+++ include/equip.h
@@ -1,62 +1,37 @@
 #ifndef EQUIP_H
 #define EQUIP_H
+
+#include <string>
 #include <iostream>
 
-class Hero;  // ⭐ 添加前向声明，避免循环引用
+class Hero;
 
+enum class EquipmentType {
+    Weapon,
+    Armor,
+    Accessory
+};
 
+class Equipment {
+private:
+    EquipmentType type;
+    std::string name;
+    unsigned int num;
+    unsigned int price;
+    unsigned int index; // 用来区分同类不同装备
 
-class Accessory {
-    protected:
-        std::string name;
-        unsigned int num = 1;
-        unsigned int price = 10; 
-    public:
-    Accessory(const std::string& name, unsigned int num, unsigned int price) {
-        this->name = name;
-        this->num = num;
-        this->price = price;
-    }
+public:
+    Equipment(EquipmentType type, const std::string& name, unsigned int num, unsigned int price, unsigned int index)
+        : type(type), name(name), num(num), price(price), index(index) {}
+
+    EquipmentType getType() const { return type; }
     std::string get_name() const { return name; }
     unsigned int get_num() const { return num; }
     unsigned int get_price() const { return price; }
-    virtual void equip(Hero* hero) const = 0;
+    unsigned int get_index() const { return index; }
 
+    // 装备行为，不再是虚函数，由 cpp 中 switch 实现
+    void equip(Hero* hero) const;
 };
-class Amulet: public Accessory {
-    public:
-    Amulet(const std::string& name, unsigned int num, unsigned int price) : Accessory(name, num, price) {}
-    void equip(Hero* hero) const override;
-    // {
-    //     std::cout << " 勇者护符已装备！" << std::endl;
-    //     hero->change_Defense(10);//需要修改成所有格挡减伤提高30%
-    // }
-};//勇者护符
-class Cape: public Accessory {
-    public:
-    Cape(const std::string& name, unsigned int num, unsigned int price) : Accessory(name, num, price) {}
-    void equip(Hero* hero) const override;
-    // {
-    //     std::cout << " 灵光披风已装备！" << std::endl;
-    //     hero->change_Speed(10);
-    // }
-};//灵光披风
-class Ring: public Accessory {
-    public:
-    Ring(const std::string& name, unsigned int num, unsigned int price) : Accessory(name, num, price) {}
-    void equip(Hero* hero) const override;
-    // {    
-    //     std::cout << " 幸运戒指已装备！" << std::endl;
-    //     hero->change_Luck(10);
-    // }
-};//幸运戒指
-
-
-
-
-
-
-
-
 
 #endif
--- main.cpp
+++ main.cpp
@@ -9,12 +9,17 @@ using namespace std;
 
 int main() {
     Hero hero("scene_color", 100,10,101,10,10,1);
-    Amulet HUFU("hupu",1,100);
-    hero.setAccessory(&HUFU);
-    HUFU.equip(&hero);
-    StatusEffect hero_status;
-    hero.setStatusEffect(&hero_status);
     hero.show_info(&hero);
+    Equipment equipment(EquipmentType::Weapon, "暴风大剑", 10, 100, 3);
+    Equipment wwwww(EquipmentType::Armor, "流云铠甲", 10, 100, 2);
+    Equipment eeeee(EquipmentType::Accessory, "1111", 10, 100, 2);
+    hero.equip_Accessory(&eeeee);
+    hero.equip_Weapon(&equipment);
+    hero.equip_Armor(&wwwww);
+    hero.show_info(&hero);
+    hero.print_Equipped();
+    // hero.setStatusEffect(&hero_status);
+    // hero.show_info(&hero);
     // Medicine medicine("haha", 10, 100);
     // Antidote antidote("hehe", 10, 100);
     // Antibiotic antibiotic("lala", 10, 100);
@@ -28,7 +33,6 @@ int main() {
     // hero.show_info(&hero);
     // antibiotic.use(&hero);
     // antidote.use(&hero);
-    hero.show_info(&hero);
     //饰品的装载
     return 0;
 }
